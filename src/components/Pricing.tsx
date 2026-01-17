89984ab79323c42e0c4a7c940e5f9b0c63003545
officialjonnyd <jonnydreach@gmail.com>
2026-01-16 19:56:00 -0700

    Updated Pricing.tsx

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -53,14 +53,8 @@ export default function Pricing() {
         </div>
       </section>
 
-      <section className="py-20 bg-white relative">
-        <div className="absolute inset-0 left-0 right-0 opacity-15 pointer-events-none">
-          <hana-viewer
-            url="https://prod.spline.design/Vht2FZbJZDv5avEL-FND/scene.hanacode"
-          ></hana-viewer>
-        </div>
-
-        <div className="container mx-auto px-4 relative z-10">
+      <section className="py-20 bg-white">
+        <div className="container mx-auto px-4">
           <div className="max-w-4xl mx-auto">
             <div className="glass-white border-l-4 border-[#7ABB00] p-6 rounded-r-2xl mb-12 shadow-xl">
               <div className="flex gap-3">
